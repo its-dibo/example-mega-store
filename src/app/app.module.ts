bd8ba439251dac625dd828308094f97f0e58858f
dibo <sherif.eldeeb@learnovia.com>
2022-12-30 11:30:16 +0200

    feat(products): register routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Route } from '@angular/router';
 import { AppComponent } from './app.component';
 
+const routes: Route[] = [
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./products/products.module').then((m) => m.ProductsModule),
+  },
+];
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,17 +3,16 @@ import { CommonModule } from '@angular/common';
 import { ListComponent } from './list/list.component';
 import { DetailsComponent } from './details/details.component';
 import { EditorComponent } from './editor/editor.component';
+import { Route } from '@angular/router';
 
-
+const routes: Route[] = [
+  { path: 'products/:id', component: DetailsComponent },
+  { path: 'products', component: ListComponent },
+  { path: 'editor', component: EditorComponent },
+];
 
 @NgModule({
-  declarations: [
-    ListComponent,
-    DetailsComponent,
-    EditorComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ListComponent, DetailsComponent, EditorComponent],
+  imports: [CommonModule],
 })
-export class ProductsModule { }
+export class ProductsModule {}
