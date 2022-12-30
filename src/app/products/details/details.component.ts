3ad35d113f7f487b833cc38d8a0eb574629483fd
dibo <sherif.eldeeb@learnovia.com>
2022-12-30 13:59:37 +0200

    feat(products): interfaces

--- src/app/products/details/details.component.ts
+++ src/app/products/details/details.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  category: string;
+  image: string;
+  rating: { rate: number; count: number };
+  link: string;
+}
+
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
+  products!: Product[];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
